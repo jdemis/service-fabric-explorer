f7081e884626ce76697020025b406519762c645e
jeffj6123 <jeffrey.jarry@gmail.com>
2022-01-06 10:57:45 -0800

    reworded partition commands

--- src/SfxWeb/src/app/Models/powershellCommands/partition.command.ts
+++ src/SfxWeb/src/app/Models/powershellCommands/partition.command.ts
@@ -9,15 +9,15 @@ export class PartitionCommandHandler extends CommandHandler {
       {
         name: 'Get-ServiceFabricPartition',
         command: `Get-ServiceFabricPartition -PartitionId ${this.partition.id}`,
-        description: 'Gets information about the partitions of a specified Service Fabric partition.'
+        description: 'Gets information about the specified Service Fabric partition.'
       },
       {
         name: 'Restart the primary replica',
         command: `Restart-ServiceFabricReplica -ReplicaKindPrimary -PartitionId ${this.partition.id} -ServiceName ${this.partition.parent.name}`,
-        description: 'Gets information about the partitions of a specified Service Fabric partition.'
+        description: 'Restarts the primary replica of the specified Service Fabric partition.'
       },
       healthReportCommand('Send-ServiceFabricPartitionHealthReport',
-      'Sends a health report on a Service Fabric service partition.',
+      'Sends a health report on the specified Service Fabric partition.',
       `Send-ServiceFabricPartitionHealthReport -PartitionId ${this.partition.id}`)
     ]
   }
